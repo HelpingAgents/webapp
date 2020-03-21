0185f9ec64bd55ed469da852bbcee878833a7d82
Niklas Portmann <niklaspor@gmail.com>
2020-03-21 12:50:21 +0100

    setup colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,22 @@ module.exports = {
 	theme: {
 		extend: {
 			colors: {
-				darkgray: '#2d3748',
+				darkgray: '#1A1A1A',
 				blue: '#20639b',
 				white: 'white',
+
+				primary: {
+					default: '#0896B6',
+					light: '#46AAC0',
+				},
+				secondary: {
+					default: '#98118A',
+					light: '#B112A2',
+				},
+				tertiary: {
+					default: '#EF9A1B',
+					light: '#FDB343',
+				},
 			},
 		},
 	},
