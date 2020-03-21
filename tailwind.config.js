6f51f1c31a1f771101ef3870dbb7625f9e2cddaa
Niklas Portmann <niklaspor@gmail.com>
2020-03-21 08:22:07 +0100

    landing draft

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 module.exports = {
-  theme: {
-    extend: {},
-  },
-  variants: {},
-  plugins: [],
-}
+	theme: {
+		extend: {},
+		colors: {
+			darkgray: '#2d3748',
+			blue: '#20639b',
+			white: 'white',
+		},
+	},
+	variants: {},
+	plugins: [],
+};
